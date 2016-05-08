d2fa6584c30cecb51465cf5c1fc85ba4edc67b43
jimhunter <jim.a.hunter@gmail.com>
2016-05-08 00:24:51 -0500

    instructions finished and highscores background set

--- HighScores.java
+++ HighScores.java
@@ -18,6 +18,6 @@ public class HighScores extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1200, 900, 1); 
         Actor back = new BackButton();
-        addObject(back, 210, 850);
+        addObject(back, 210, 830);
     }
 }
