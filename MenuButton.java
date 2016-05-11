a23743bbcece6623baabbe021c89452ee3c80a2e
jimhunter <jim.a.hunter@gmail.com>
2016-05-10 20:13:55 -0500

    sorry more updates

--- MenuButton.java
+++ MenuButton.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MenuButton here.
  * 
- * @author (your name) 
+ * @author Jim Hunter
  * @version (a version number or a date)
  */
 public class MenuButton extends Buttons
